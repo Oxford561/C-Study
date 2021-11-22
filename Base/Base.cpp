75180c26b10cfdfef499b7a161ae0c2dff5c4d4e
GAME\wb.liujin01 <2068699719@qq.com>
2021-11-22 14:50:11 +0800

    了解数组

--- Base/Base.cpp
+++ Base/Base.cpp
@@ -12,6 +12,7 @@ void TestVar();
 void TestType();
 void TestOperator();
 void TestProcedure();
+void TestArray();
 
 int main()
 {
@@ -24,6 +25,8 @@ int main()
     //TestOperator();
     // 测试程序流程
     //TestProcedure();
+    // 测试数组
+    TestArray();
 
     system("pause");// 单行注释 让命令提示框不至于闪现
     return 0;
@@ -459,3 +462,86 @@ void TestProcedure()
     cout << "5" << endl;
 }
 
+void TestArray()
+{
+    // 数组的定义
+    int score[10];
+    score[0] = 100;
+    score[1] = 99;
+    score[2] = 85;
+
+    cout << score[0] << endl;
+    cout << score[1] << endl;
+    cout << score[2] << endl;
+
+    int score2[10] = { 100,90,80,70,60,50,40,30,20,10 };
+    for (int i = 0; i < 10;  i++)
+    {
+        cout << score2[i] << endl;
+    }
+
+    int score3[] = { 100,90,80,70,60,50,40,30,20,10 };
+    for (int i = 0; i < 10; i++)
+    {
+        cout << score3[i] << endl;
+    }
+
+    //数组名用途
+    //1、可以获取整个数组占用内存空间大小
+    int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+
+    cout << "整个数组所占内存空间为： " << sizeof(arr) << endl;
+    cout << "每个元素所占内存空间为： " << sizeof(arr[0]) << endl;
+    cout << "数组的元素个数为： " << sizeof(arr) / sizeof(arr[0]) << endl;
+
+    //2、可以通过数组名获取到数组首地址
+    cout << "数组首地址为： " << (int)arr << endl;
+    cout << "数组中第一个元素地址为： " << (int)&arr[0] << endl;
+    cout << "数组中第二个元素地址为： " << (int)&arr[1] << endl;
+
+    //arr = 100; 错误，数组名是常量，因此不可以赋值
+
+    // 二维数组
+    int arr2[2][3];
+    arr2[0][0] = 1;
+    arr2[0][1] = 2;
+    arr2[0][2] = 3;
+    arr2[1][0] = 4;
+    arr2[1][1] = 5;
+    arr2[1][2] = 6;
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << arr2[i][j] << endl;
+        }
+        cout << endl;
+    }
+
+    int arr3[2][3] = {
+        {1,2,3},
+        {4,5,6}
+    };
+
+    int arr4[2][3] = { 1,2,3,4,5,6 };
+
+    int arr5[][3] = { 1,2,3,4,5,6 };
+
+    cout << "二维数组大小： " << sizeof(arr2) << endl;
+    cout << "二维数组一行大小： " << sizeof(arr2[0]) << endl;
+    cout << "二维数组元素大小： " << sizeof(arr2[0][0]) << endl;
+
+    cout << "二维数组行数： " << sizeof(arr2) / sizeof(arr2[0]) << endl;
+    cout << "二维数组列数： " << sizeof(arr2[0]) / sizeof(arr2[0][0]) << endl;
+
+    //地址
+    cout << "二维数组首地址：" << arr2 << endl;
+    cout << "二维数组第一行地址：" << arr2[0] << endl;
+    cout << "二维数组第二行地址：" << arr2[1] << endl;
+
+    cout << "二维数组第一个元素地址：" << &arr2[0][0] << endl;
+    cout << "二维数组第二个元素地址：" << &arr2[0][1] << endl;
+
+
+}
