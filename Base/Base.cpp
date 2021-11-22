6ce71de83694f8a6758e9fd37e3a81b8abf56cf1
GAME\wb.liujin01 <2068699719@qq.com>
2021-11-22 10:24:23 +0800

    了解注释，变量，常量

--- Base/Base.cpp
+++ Base/Base.cpp
@@ -4,9 +4,29 @@
 #include <iostream>
 using namespace std;
 
+// 定义常量：宏常量
+#define day 7
+
 int main()
 {
+    // 通过 cout 输出字符串到控制台
+    /*
+       这是多行注释
+       是
+       是
+    */
     cout << "Hello World!\n";
-    system("pause");//让命令提示框不至于闪现
+
+    // 变量的定义
+    int a = 10;
+    cout << "a= " << a << endl;
+
+    // 常量的定义 ,常量不可以修改
+    cout << "一周总共有" << day << "天" << endl;
+    // 定义常量：const 修饰变量
+    const int month = 12;
+    cout << "一年里总共有" << month << "个月份" << endl;
+
+    system("pause");// 单行注释 让命令提示框不至于闪现
     return 0;
 }
