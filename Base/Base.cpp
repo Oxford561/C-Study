bc4246367f6cc131138003cdea3b0bee5562165f
GAME\wb.liujin01 <2068699719@qq.com>
2021-11-22 10:13:46 +0800

    创建基础C++工程，实现 HelloWorld

---
+++ Base/Base.cpp
@@ -0,0 +1,12 @@
+﻿// Base.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    system("pause");//让命令提示框不至于闪现
+    return 0;
+}
